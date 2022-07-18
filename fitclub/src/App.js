dbf681fe4cf77462aef00a3829762aa72af96656
Pedro Gazola <ph-gazola@hotmail.com>
2022-07-18 01:22:15 -0300

    Reasons

--- fitclub/src/App.js
+++ fitclub/src/App.js
@@ -1,9 +1,13 @@
 import Hero from './components/Hero/hero';
 import './App.css';
+import Programs from './components/Programs/programs';
+import Reasons from './components/Reasons/reasons';
 function App() {
   return (
     <div className="App">
           <Hero />
+          <Programs />
+          <Reasons />
     </div>
   );
 }
